53e990598e019510a6fbeffe2014486d92a2a535
leedMx <esparza.mexico@gmail.com>
2023-01-30 03:29:25 -0600

    Removed unused errorhandler

--- src/App.js
+++ src/App.js
@@ -6,11 +6,6 @@ import UsersList from './components/Users/UsersList';
 function App() {
   const [users, setUsers] = useState([]);
 
-
-  const errorMessageHandler = (message) => {
-    console.log(message);
-  }
-
   const addUserHandler = (user) => {
     setUsers( prevUsers => { return [...prevUsers, user]})
   }
